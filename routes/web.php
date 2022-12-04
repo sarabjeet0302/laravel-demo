5eed6fa5df960026f925baa63e2d9fe1cb20f1bd
Owner <owner@Owners-MacBook-Pro.local>
2022-12-03 17:37:48 -0800

    list categories & view products page integration

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,24 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+
+//Route::get('list/category', function () {
+
+   //$categories = DB::table('categories')->get();
+   //return view('list-category',compact('categories'));
+
+//});
+
+Route::get('list/category','App\Http\Controllers\CategoryController@index');
+
+Route::get('list/products/{categoryId}', 'App\Http\Controllers\ProductController@index');
+
+
+
+
